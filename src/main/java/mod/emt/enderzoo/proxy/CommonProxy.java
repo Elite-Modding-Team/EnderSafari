99efe05a9c76fc65bfeff7e9ec07a2dedc6ae243
IcarussOne <135455255+IcarussOne@users.noreply.github.com>
2026-07-09 00:39:21 -0500

    Add JER support

--- src/main/java/mod/emt/enderzoo/proxy/CommonProxy.java
+++ src/main/java/mod/emt/enderzoo/proxy/CommonProxy.java
@@ -1,6 +1,8 @@
 package mod.emt.enderzoo.proxy;
 
+import mod.emt.enderzoo.compat.JERIntegration;
 import mod.emt.enderzoo.registry.ModLootTablesEZ;
+import utils.helpers.CompatHelper;
 
 public class CommonProxy {
     public void preInit() {
@@ -8,6 +10,10 @@ public class CommonProxy {
 
     public void init() {
         ModLootTablesEZ.registerLootTables();
+
+        if (CompatHelper.isJERLoaded) {
+            JERIntegration.init();
+        }
     }
 
     public void postInit() {
---
+++ src/main/java/utils/helpers/CompatHelper.java
@@ -0,0 +1,7 @@
+package utils.helpers;
+
+import net.minecraftforge.fml.common.Loader;
+
+public class CompatHelper {
+    public static final boolean isJERLoaded = Loader.isModLoaded("jeresources");
+}
