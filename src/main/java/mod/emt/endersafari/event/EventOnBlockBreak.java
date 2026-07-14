dd0afeaa2f05097a949965b951531ad89ab97833
IcarussOne <135455255+IcarussOne@users.noreply.github.com>
2026-07-14 01:33:45 -0500

    Restrict dire cube hardcore spawning on creative

--- src/main/java/mod/emt/endersafari/event/EventOnBlockBreak.java
+++ src/main/java/mod/emt/endersafari/event/EventOnBlockBreak.java
@@ -7,6 +7,7 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -21,9 +22,18 @@ public class EventOnBlockBreak {
         if (!world.isRemote) {
             IBlockState state = event.getState();
             ItemStack heldStack = event.getPlayer().getHeldItemMainhand();
-            if (isValidBlock(state) && !isValidStack(state, heldStack) && world.rand.nextFloat() <= ESConfig.ENTITIES.DIRE_CUBE.hardcoreSpawningChance) {
+            if (!event.getPlayer().capabilities.isCreativeMode && isValidBlock(state) && !isValidStack(state, heldStack) && world.rand.nextFloat() <= ESConfig.ENTITIES.DIRE_CUBE.hardcoreSpawningChance) {
                 BlockPos pos = event.getPos();
                 EntityDireCube cube = new EntityDireCube(world);
+
+                if (BiomeDictionary.hasType(cube.getEntityWorld().getBiome(cube.getPosition()), BiomeDictionary.Type.MESA)) {
+                    cube.setType(3); // Red Sand
+                } else if (BiomeDictionary.hasType(cube.getEntityWorld().getBiome(cube.getPosition()), BiomeDictionary.Type.SANDY)) {
+                    cube.setType(2); // Sand
+                } else if (BiomeDictionary.hasType(cube.getEntityWorld().getBiome(cube.getPosition()), BiomeDictionary.Type.SNOWY) || BiomeDictionary.hasType(cube.getEntityWorld().getBiome(cube.getPosition()), BiomeDictionary.Type.MOUNTAIN)) {
+                    cube.setType(1); // Gravel
+                } // Otherwise just Dirt
+
                 cube.setSlimeSize(1, true);
                 cube.setPosition(pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5);
                 world.spawnEntity(cube);
