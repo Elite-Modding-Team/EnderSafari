bbb3795468010d01deea161e149faee82b53ee8d
IcarussOne <135455255+IcarussOne@users.noreply.github.com>
2026-07-27 10:50:56 -0500

    Add glow layers

--- src/main/java/mod/emt/endersafari/client/render/RenderEnderChicken.java
+++ src/main/java/mod/emt/endersafari/client/render/RenderEnderChicken.java
@@ -1,6 +1,7 @@
 package mod.emt.endersafari.client.render;
 
 import mod.emt.endersafari.EnderSafari;
+import mod.emt.endersafari.client.render.layer.LayerGlow;
 import mod.emt.endersafari.entity.EntityEnderChicken;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderChicken;
@@ -11,10 +12,12 @@ import net.minecraftforge.fml.client.registry.IRenderFactory;
 import org.jetbrains.annotations.NotNull;
 
 public class RenderEnderChicken extends RenderChicken {
-    private static final ResourceLocation TEXTURE = new ResourceLocation(EnderSafari.MOD_ID, "textures/entity/ender_chicken.png");
+    private static final ResourceLocation TEXTURE_GLOW = new ResourceLocation(EnderSafari.MOD_ID, "textures/entity/ender_chicken/ender_chicken_eyes.png");
+    private static final ResourceLocation TEXTURE = new ResourceLocation(EnderSafari.MOD_ID, "textures/entity/ender_chicken/ender_chicken.png");
 
     public RenderEnderChicken(RenderManager renderManager) {
         super(renderManager);
+        this.addLayer(new LayerGlow<>(this, TEXTURE_GLOW));
     }
 
     @Override
---
+++ src/main/java/mod/emt/endersafari/client/render/layer/LayerGlow.java
@@ -0,0 +1,46 @@
+package mod.emt.endersafari.client.render.layer;
+
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.client.renderer.entity.layers.LayerRenderer;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import org.jetbrains.annotations.NotNull;
+
+@SideOnly(Side.CLIENT)
+public class LayerGlow<T extends EntityLiving> implements LayerRenderer<T> {
+    private final RenderLiving<T> renderer;
+    private final ResourceLocation texture;
+
+    public LayerGlow(RenderLiving<T> renderer, ResourceLocation texture) {
+        this.renderer = renderer;
+        this.texture = texture;
+    }
+
+    @Override
+    public void doRenderLayer(@NotNull T entity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
+        this.renderer.bindTexture(texture);
+        GlStateManager.enableBlend();
+        GlStateManager.blendFunc(GlStateManager.SourceFactor.ONE, entity.isInvisible() ? GlStateManager.DestFactor.ONE : GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+        GlStateManager.disableLighting();
+        GlStateManager.enablePolygonOffset();
+        GlStateManager.doPolygonOffset(-0.1F, -1.0F);
+        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240.0F, 240.0F);
+        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+        this.renderer.getMainModel().render(entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
+        this.renderer.setLightmap(entity);
+        GlStateManager.doPolygonOffset(0.0F, 0.0F);
+        GlStateManager.disablePolygonOffset();
+        GlStateManager.depthMask(true);
+        GlStateManager.enableLighting();
+        GlStateManager.disableBlend();
+    }
+
+    @Override
+    public boolean shouldCombineTextures() {
+        return true;
+    }
+}
